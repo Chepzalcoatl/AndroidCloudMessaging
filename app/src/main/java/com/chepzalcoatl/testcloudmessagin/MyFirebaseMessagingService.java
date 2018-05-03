2582858fbf358defec01f0ce3d273a7c04a24cba
Eduardo Barrón <>
2018-05-03 13:42:08 -0500

    Ahora recibe notificaciones.

---
+++ app/src/main/java/com/chepzalcoatl/testcloudmessagin/MyFirebaseMessagingService.java
@@ -0,0 +1,25 @@
+package com.chepzalcoatl.testcloudmessagin;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseMessagingService extends FirebaseInstanceIdService {
+
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FB_NOTIFICACIONES", "Refreshed token: " + refreshedToken);
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+
+}
